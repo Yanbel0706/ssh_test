e7c033a93280762dd79837b8abf6197de5b00f8d
yanbel0706 <yanis.belk.edu@gmail.com>
2023-11-15 21:00:34 +0100

    betty for change_dir

--- change_dir.c
+++ change_dir.c
@@ -1,37 +1,36 @@
 #include "shell.h"
-/**                                                                                                      
-** change_dir - changes directory                                                                       
-** @args: directory path                                                                                
-**/                                                                                                    
-void change_dir(char **args) 
-{                                                                           
-   char *new_dir;
-   char *pwd;
-    if (args[1] == NULL)
-         {
-               new_dir = getenv("HOME");
-         }
-     else
-         {
-               new_dir = args[1];
-         }
-    
-    
-     if (chdir(new_dir) != 0)
-         {
-             perror("cd"); 
-         }
-     else
-         {
-   
-           pwd = getcwd(NULL, 0);
-           if (pwd != NULL)
-                                                                                                                 {
-               setenv("PWD",pwd, 1);                                                                                   free(pwd);                             
-														 }
-                                                                                                                else
-                                                                                                                 {
-               perror("getcwd");
-                                                                                                                 }
-                                                                                                                 }
-                                                                                                        }
+/**
+** change_dir - changes directory
+** @args: directory path
+**/
+void change_dir(char **args)
+{
+	char *new_dir;
+	char *pwd;
+
+	if (args[1] == NULL)
+	{
+		new_dir = getenv("HOME");
+	}
+	else
+	{
+		new_dir = args[1];
+	}
+	if (chdir(new_dir) != 0)
+	{
+		perror("cd");
+	}
+	else
+	{
+		pwd = getcwd(NULL, 0);
+		if (pwd != NULL)
+		{
+			setenv("PWD", pwd, 1);
+			free(pwd);
+		}
+		else
+		{
+			perror("getcwd");
+		}
+	}
+}
